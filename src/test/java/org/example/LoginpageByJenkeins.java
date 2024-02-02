1b6cff0b5a0b31ceb4e90f2f4616c1c96b1279ac
Neharana17 <neharana17feb97@gmail.com>
2024-02-02 10:29:03 +0530

    testng file

--- src/test/java/org/example/LoginpageByJenkeins.java
+++ src/test/java/org/example/LoginpageByJenkeins.java
@@ -29,8 +29,22 @@ public class LoginpageByJenkeins {
     }
 
 
-        @Test(priority = 1,groups = {"nagitive", "sanity","reg"})
-                public  void login(){
+        @Test(priority = 0,groups = {"Nagetive", "sanity","stage"})
+                public  void login1(){
+        driver.get("https://app.vwo.com/#/login");
+        driver.findElement(By.id("login-username")).sendKeys("auto123@inclusion.com");
+        driver.findElement(By.id("login-password")).sendKeys("Neha172#");
+        driver.findElement(By.id("js-login-btn")).click();
+        driver.findElement(By.id("login-username")).clear();
+
+//        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h1.page-heading")));
+//        Assert.assertEquals(driver.getTitle(), "Dashboard");
+//        Assert.assertEquals(driver.getCurrentUrl(), "https://app.vwo.com/#/dashboard");
+//        driver.get("https://app.vwo.com/logout");
+
+    }
+    @Test(priority = 1,groups = {"Postive", "sanity","reg"})
+    public  void login2() {
         driver.get("https://app.vwo.com/#/login");
         driver.findElement(By.id("login-username")).sendKeys("auto123@inclusion.com");
         driver.findElement(By.id("login-password")).sendKeys("Neha1702#");
